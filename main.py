e54339577e188ebf0e1fcc90a17a45033bddd8ef
Gabriella DiVentura <gabrielladiv@gmail.com>
2024-05-16 10:34:16 -0400

    Test writing to md

--- main.py
+++ main.py
@@ -44,11 +44,11 @@ def generate_content_line(title: str, date: str, url_path: str) -> str:
 
 def insert_to_content_page(title: str, date: str, file_name: str):
     """Insert the markdown content into the content page"""
-    with open('docs/index.md', 'w') as file:
+    with open(CONTENT_DIR, 'w') as file:
      file.write("# XKCD Comics\n")
      file.write(CONTENT_PAGE_SPLIT)
 
-    with open('docs/index.md', 'r') as file:
+    with open(CONTENT_DIR, 'r') as file:
      data = file.read()
 
     if CONTENT_PAGE_SPLIT in data:
